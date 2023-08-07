226b603d8217be14b71fadf3c4fc28e1b20984ea
zhongming <atenliao@gmail.com>
2023-08-06 23:36:34 -0700

    complete oop

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,31 @@
 #!/usr/bin/env python3
 
 class Book:
+    page_count =0
+    def __init__(self,title, page_count):
+        self.title=title
+        self._page_count =page_count
+        if(type(self.page_count) is int):
+            print("it is integer")
+        else:
+            print("page_count must be an integer")
+        
+    @property
+    def page_count(self):
+        print("get page_count")
+        return self._page_count
+    
+    @page_count.setter
+    def page_count(self, value):
+        if(type(value) != int):
+            print("page_count must be an integer")
+        else:
+            print("it is integer")
+        self._page_count = value
+        
+        
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
     pass
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    condition = None
+    def __init__(self, brand, size) -> None:
+        self.brand =brand
+        self.size = size
+        Shoe.condition = "New"
+        pass
+    
+
+    @property
+    def size(self):
+        return self._size
+    
+    @size.setter
+    def size(self, value):
+        if(type(value) != int):
+            print("size must be an integer")
+        self._size = value
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
